357207654b6d4d3f7f7bf2e3fcf9805760c15e5b
Grégoire Morpain <gregoire.morpain@gmail.com>
2021-12-17 12:04:50 +0100

    It compiles! Ship it!

--- config/lsp-config.lua
+++ config/lsp-config.lua
@@ -170,9 +170,8 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
   }
 )
 
-local signs = {Error = " ", Warning = " ", Hint = " ", Information = " "}
-
+local signs = {Error = " ", Warn = " ", Hint = " ", Info = " "}
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
-  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
 end
