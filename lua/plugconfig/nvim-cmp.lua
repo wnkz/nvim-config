8293d39bed76f66d7ee5af7473121d5c7ecfe5e5
Grégoire Morpain <gregoire.morpain@gmail.com>
2022-06-21 11:22:15 +0200

    perfect...

--- lua/plugconfig/nvim-cmp.lua
+++ lua/plugconfig/nvim-cmp.lua
@@ -118,7 +118,13 @@ cmp.setup.cmdline(
 require("luasnip.loaders.from_vscode").load()
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({map_char = {tex = ""}}))
-
--- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
-cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
+cmp.event:on(
+  "confirm_done",
+  cmp_autopairs.on_confirm_done(
+    {
+      filetypes = {
+        tex = false
+      }
+    }
+  )
+)
