e71c6bc6f41612bc40c606ef23ff6f78ca2f3e00
Grégoire Morpain <gregoire.morpain@gmail.com>
2022-07-26 15:37:28 +0200

    this is Spartaaaaaaaa

--- lua/plugconfig/dashboard.lua
+++ lua/plugconfig/dashboard.lua
@@ -1,5 +1,7 @@
+local home = os.getenv('HOME')
 local db = require("dashboard")
 
+db.session_directory = home .. "/.cache/vim/session"
 db.preview_file_height = 12
 db.preview_file_width = 80
 db.custom_center = {
