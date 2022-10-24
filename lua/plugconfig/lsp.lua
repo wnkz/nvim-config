bc61b9dfba391e0b09b9854af6cb7bcc3239eac7
Grégoire Morpain <gregoire.morpain@gmail.com>
2022-10-24 11:50:02 +0200

    Never gonna say goodbye

--- lua/plugconfig/lsp.lua
+++ lua/plugconfig/lsp.lua
@@ -53,7 +53,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Add additional capabilities supported by nvim-cmp
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local enhance_server_opts = {
   ["pyright"] = function(opts)
--- lua/plugconfig/nvim-cmp.lua
+++ lua/plugconfig/nvim-cmp.lua
@@ -88,7 +88,7 @@ cmp.setup.filetype(
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(
-  "/",
+  {"/", "?"},
   {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources(
