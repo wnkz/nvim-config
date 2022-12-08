bf21a1776fa4aeeecd3dbd77859525b563a4cebd
Grégoire Morpain <gregoire.morpain@gmail.com>
2022-12-08 16:05:59 +0100

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- lua/plugconfig/indent-blankline.lua
+++ lua/plugconfig/indent-blankline.lua
@@ -4,5 +4,5 @@ require("indent_blankline").setup {
     show_current_context = true,
     use_treesitter = true,
     buftype_exclude = {"terminal"},
-    filetype_exclude = {"dashboard", "alpha", "lsp-installer", "mason"}
+    filetype_exclude = {"dashboard", "alpha", "lsp-installer", "mason", "packer"}
 }
