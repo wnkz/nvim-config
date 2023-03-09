3758f833911daf70cb45722b3d8f2ac29a6d8b50
Grégoire Morpain <gregoire.morpain@gmail.com>
2023-03-09 20:02:36 +0100

    fix: Telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,32 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         branch = "0.1.x",
+        config = function()
+            local actions = require("telescope.actions")
+            require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<esc>"] = actions.close,
+                        },
+                    },
+                },
+                extensions = {
+                    fzf = {
+                        fuzzy = true,
+                        override_generic_sorter = false,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                    },
+                },
+            })
+
+            require("telescope").load_extension("fzf")
+            require("telescope").load_extension("gh")
+            require("telescope").load_extension("file_browser")
+            require("telescope").load_extension("ui-select")
+            require("telescope").load_extension("frecency")
+        end,
         cmd = "Telescope",
         dependencies = {
             "telescope-fzf-native.nvim",
