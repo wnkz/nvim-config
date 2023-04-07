ba75b82442013d5e4126dd8b6f230532ee489e0a
Grégoire Morpain <gregoire.morpain@gmail.com>
2023-04-07 20:19:18 +0200

    feat: ft* vim > lua

---
+++ ftdetect/mine.lua
@@ -0,0 +1,12 @@
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "~/.aws/config", "~/.aws/credentials" },
+    command = "set filetype=dosini",
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "*.conf" },
+    command = "set filetype=conf",
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "Jenkinsfile" },
+    command = "set filetype=groovy",
+})
--- ftdetect/mine.vim
+++
@@ -1,3 +0,0 @@
-au BufRead,BufNewFile ~/.aws/config set filetype=dosini
-au BufRead,BufNewFile ~/.aws/credentials set filetype=dosini
-au BufRead,BufNewFile Jenkinsfile set filetype=groovy
