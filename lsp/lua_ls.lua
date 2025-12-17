56f57a4c2339aa83372ff1f499661363bf6070f0
Grégoire `wnkz` M. <wnkz@users.noreply.github.com>
2025-12-17 18:35:42 +0100

    feat: Lua settings

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,10 +1,6 @@
 return {
   settings = {
     Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
-      },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
         enable = false,
